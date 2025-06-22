b1cee4047f3378d17ba54051807b28f721b64a5a
nk747 <nkdiwedi17@gmail.com>
2025-06-22 10:56:07 +0530

    Update index.jsfix: added working React App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';  // ✅ App with capital A
 import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);fix: correct App import in index.js
+);
