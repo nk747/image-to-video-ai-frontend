383e11253abf10b0bbd837187e74539650b1f210
nk747 <nkdiwedi17@gmail.com>
2025-06-22 10:45:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // ✅ App with capital A
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
